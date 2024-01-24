99592738d45a9f2d15f23af39f1e0899c9edd77a
00tsvetelina <142497512+00tsvetelina@users.noreply.github.com>
2024-01-24 16:12:48 +0200

    first commit; created db and established connection

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.example.utils.DBUtil;
+
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DBUtil.getConnection();
+
+    }
+
+}
---
+++ src/main/java/org/example/utils/DBUtil.java
@@ -0,0 +1,27 @@
+package org.example.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DBUtil {
+    private static Connection instance;
+
+    private DBUtil() {}
+
+    public static Connection getConnection() {
+
+        if(instance == null){
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                instance = DriverManager.getConnection("jdbc:mysql://localhost:3300", "root", "123456");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.exit(0);
+            }
+        }
+        
+        return instance;
+
+    }
+
+}
